eae8039879012941f17b33b9474e69dbbd8f81c9
Dasol Baek <schgana@hotmail.com>
2018-11-14 18:12:07 +1300

    working on getting api..

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  state = {
+    model:'default',
+    input: [[44,43,44],[90,83,82],"N","N","N"]
+  }
+
+  componentDidMount() {
+    const url = 'http://colormind.io/list/'
+
+
+    request.post(url)
+    .send(this.state)
+    .then(res => {
+      this.setState(res.body)
+      //let palette = JSON.parse(XMLHttpRequest.responseText).result
+      console.log(res.body)
+    })
+    .catch(err => console.err(err))
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>HI</h1>
+        {this.state}
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
